8a235d29a45de5ccd29eb598c08d7498477737b0
Medill-East <NEMailLHD@163.com>
2020-02-18 11:10:27 +0800

    70. Climbing Stairs
    20200218 斐波那契

---
+++ 70. Climbing Stairs.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    //20200218 斐波那契
+    public int ClimbStairs(int n) {
+        //F(n) = F(n-1) + F(n-2)
+        if (n<3)
+        {
+            return n;
+        }
+        else
+        {
+            int a =1, b=2, temp;
+            for (int i = 3; i <= n; i++)
+            {
+                temp = a;
+                a = b;
+                b = temp+b;
+            }
+            return b;
+        }
+    }
+
+}
