3b585385745631798b670f6d758435a83fc12e8f
Medill-East <NEMailLHD@163.com>
2021-04-30 00:34:45 +0200

    Create 26. Remove Duplicates from Sorted Array.cs
    202104300034

---
+++ 26. Remove Duplicates from Sorted Array.cs
@@ -0,0 +1,28 @@
+﻿public class Solution26
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        if (nums.Length == 0)
+        {
+            return 0;
+        }
+        else if (nums.Length == 1)
+        {
+            return 1;
+        }
+        else
+        {
+            int i = 0;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i] != nums[j])
+                {
+                    nums[i + 1] = nums[j];
+                    i++;
+                }
+            }
+            return i + 1;
+        }
+
+    }
+}
